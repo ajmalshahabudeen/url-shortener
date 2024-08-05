ed365b3c010ca62301590b5c8d88b9d96585fd43
僕は神だ <ajmalshahabudeen@gmail.com>
2024-08-05 18:13:29 +0530

    find url api

--- server/api/dbActions/url/find.get.ts
+++ server/api/dbActions/url/find.get.ts
@@ -1,3 +1,41 @@
+import { db } from "~/utils/db";
+
 export default defineEventHandler(async (event) => {
-  return 'Hello Nitro'
-})
+  const query = getQuery(event);
+  const key = query.key;
+
+  if (key) {
+    console.log(key);
+
+    try {
+      const res = await db.query.url.findMany({
+        where: (url, { eq }) => eq<any>(url.url_key, key),
+      });
+
+      if (res.length == 0) {
+        setResponseStatus(event, 404);
+        return {
+          message: "Failed to find URL",
+          value: null,
+        };
+      } else {
+        setResponseStatus(event, 200);
+        return {
+          message: "Found URL",
+          value: res,
+        };
+      }
+    } catch (err) {
+      setResponseStatus(event, 404);
+      return {
+        message: "Failed to find URL",
+        value: null,
+      };
+    }
+  }
+  setResponseStatus(event, 404);
+  return {
+    message: "Failed to find URL",
+    value: null,
+  };
+});
