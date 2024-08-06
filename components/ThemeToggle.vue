eb5b45c2bd0e1f0a3bbd87037dba18770aec0d94
僕は神だ <ajmalshahabudeen@gmail.com>
2024-08-06 15:29:14 +0530

    theme toggle icon chage

--- components/ThemeToggle.vue
+++ components/ThemeToggle.vue
@@ -1,19 +1,23 @@
 <script setup lang="ts">
-const colorMode = useColorMode()
+const colorMode = useColorMode();
 const isDark = computed({
-  get () {
-    return colorMode.value === 'dark'
+  get() {
+    return colorMode.value === "dark";
   },
-  set () {
-    colorMode.preference = colorMode.value === 'dark' ? 'light' : 'dark'
-  }
-})
+  set() {
+    colorMode.preference = colorMode.value === "dark" ? "light" : "dark";
+  },
+});
 </script>
 
 <template>
   <ClientOnly>
     <UButton
-      :icon="isDark ? 'i-heroicons-moon-20-solid' : 'i-heroicons-sun-20-solid'"
+      :icon="
+        isDark
+          ? 'line-md:sunny-filled-loop-to-moon-filled-loop-transition'
+          : 'line-md:sunny-outline-twotone-loop'
+      "
       color="gray"
       variant="ghost"
       aria-label="Theme"
--- components/UserHead.vue
+++ components/UserHead.vue
@@ -1,39 +1,54 @@
 <script setup lang="ts">
 const items = [
-  [{
-    label: 'ben@example.com',
-    slot: 'account',
-    disabled: true
-  }], [{
-    label: 'Settings',
-    icon: 'i-heroicons-cog-8-tooth'
-  }], [{
-    label: 'Documentation',
-    icon: 'i-heroicons-book-open'
-  }, {
-    label: 'Changelog',
-    icon: 'i-heroicons-megaphone'
-  }, {
-    label: 'Status',
-    icon: 'i-heroicons-signal'
-  }], [{
-    label: 'Sign out',
-    icon: 'i-heroicons-arrow-left-on-rectangle'
-  }]
-]
+  [
+    {
+      label: "ben@example.com",
+      slot: "account",
+      disabled: true,
+    },
+  ],
+  [
+    {
+      label: "Settings",
+      icon: "i-heroicons-cog-8-tooth",
+    },
+  ],
+  [
+    {
+      label: "Documentation",
+      icon: "i-heroicons-book-open",
+    },
+    {
+      label: "Changelog",
+      icon: "i-heroicons-megaphone",
+    },
+    {
+      label: "Status",
+      icon: "i-heroicons-signal",
+    },
+  ],
+  [
+    {
+      label: "Sign out",
+      icon: "i-heroicons-arrow-left-on-rectangle",
+    },
+  ],
+];
 </script>
 
 <template>
-  <UDropdown :items="items" :ui="{ item: { disabled: 'cursor-text select-text' } }" :popper="{ placement: 'bottom-start' }">
-    <div class="hover:opacity-75">
+  <UDropdown
+    :items="items"
+    :ui="{ item: { disabled: 'cursor-text select-text' } }"
+    :popper="{ placement: 'bottom-start' }"
+  >
+    <div class="hover:opacity-75 pt-2">
       <Icon name="line-md:account" size="24" />
     </div>
 
     <template #account="{ item }">
       <div class="text-left">
-        <p>
-          Signed in as
-        </p>
+        <p>Signed in as</p>
         <p class="truncate font-medium text-gray-900 dark:text-white">
           {{ item.label }}
         </p>
@@ -43,8 +58,10 @@ const items = [
     <template #item="{ item }">
       <span class="truncate">{{ item.label }}</span>
 
-      <UIcon :name="item.icon" class="flex-shrink-0 h-4 w-4 text-gray-400 dark:text-gray-500 ms-auto" />
+      <UIcon
+        :name="item.icon"
+        class="flex-shrink-0 h-4 w-4 text-gray-400 dark:text-gray-500 ms-auto"
+      />
     </template>
   </UDropdown>
 </template>
-
